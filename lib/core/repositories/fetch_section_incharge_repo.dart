606da270b5c53163f7850a1d95f575b9301132fb
mohammedakbark <akbarmohd00123@gmail.com>
2024-11-18 19:09:22 +0530

    api calling is done

---
+++ lib/core/repositories/fetch_section_incharge_repo.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_managment/core/database/auth_db.dart';
+import 'package:test_managment/model/response_model.dart';
+import 'package:test_managment/core/utils/app_const.dart';
+
+class FetchSectionInchargeRepo {
+  Dio dio = Dio();
+  final url = '${AppConst.baseURL}${AppConst.sectionInchargeURL}';
+
+  Future<ResponseModel?> fetchSectionIncharge(BuildContext context) async {
+    try {
+      final token =
+          await Provider.of<AuthDb>(context, listen: false).getUserData();
+      final response = await dio.post(url,
+          options: Options(headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+          }));
+
+      final decodedata = jsonDecode(response.toString());
+
+      if (decodedata['status'] == 200) {
+        return ResponseModel.fromJson(decodedata);
+      } else {
+        return ResponseModel.fromJson(decodedata);
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+}
