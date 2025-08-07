93d9b73715bb694b2e363359c04c795406f68862
mohammedakbark <makbarkozhikkal@gmail.com>
2025-08-07 14:29:00 +0530

    upgraded sdk to 35 and made more changes in android files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:developer';
 
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
